4adba27b7160cd785fb4d272397b4f3138504261
Maija Barrins <maija.barrins@gmail.com>
2019-04-26 12:24:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,5 @@ def reverse_each_word(string)
   # string.split(" ").each {|word| words << word.reverse}
   # words.join(" ")
 
-  string.split(" ").map{|word| word.reverse}.join(" ")
+  string.split(" ").collect{|word| word.reverse}.join(" ")
 end
